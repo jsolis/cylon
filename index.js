d453691cdc0550c028d0c04aab0d533ed3387368
Jason Solis <jsolis@gmail.com>
2015-08-23 09:54:14 -0400

    added cylon http api package and started cylon api

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var Cylon = require('cylon');
 
+// Initialize the api
+Cylon.api('http');
+
 // Initialize the robot
 Cylon.robot({
+  name: 'dorito',
+
   connections: {
     spark: { adaptor: 'spark', accessToken: '906b49385e4a37206a226033500a4e3e20bbeef3', deviceId: '29003e000747343339373536' }
   },
@@ -10,7 +15,12 @@ Cylon.robot({
     led: { driver: 'led', pin: 'D7'}
   },
 
-  work: function(my) {
-    every((1).second(), function() {my.led.toggle()});
+  //work: function(my) {
+  //  every((1).second(), function() {my.led.toggle()});
+  //}
+
+  toggle: function() {
+    this.led.toggle();
   }
+
 }).start();
